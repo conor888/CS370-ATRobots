68ba92f9ff508080dc400ddf82b3058f33c4a226
Conor Ahern <35696968+conorahern7@users.noreply.github.com>
2018-02-05 12:05:06 -0500

    Remove PushButton and fix initial encode function
    Encode function does not encode properly yet. Remove PushButton from GUI.

--- ATRLOCK/ATRLOCK.cpp
+++ ATRLOCK/ATRLOCK.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-#include <string>
+//#include <string>
 #include <fstream>
 #include <ctime>
+#include <cstring>
 
 using namespace std;
 
 #define lock_type 3
 
-int write_line(string, string, ofstream);
+int write_line(string, string, ofstream &, int, char []);
 bool file_exists(string);
 bool endsWith(string, string);
 string replaceExt(string, string);
 string ucase(string);
 string prepare(string, string);
+string encode(string, char []);
 
 int main(int argc, char *argv[]) {
-    string fn1, fn2, s, s1, s2, lock_code;
-    int i, j, k, lock_pos, lock_dat, this_dat;
+    string fn1, fn2, s, s1, s2;
+    int i, k, lock_pos, lock_dat, this_dat;
     ifstream f1;
     ofstream f2;
     time_t now = time(0);
@@ -91,35 +93,61 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    //Randomize = rand.time null
+    srand (time(NULL));
+
     //LOCK HEADER
     f2 << endl;
     f2 <<";------------------------------------------------------------------------------\n";
-    // writeln(f2,'; ',no_path(base_name(fn1)),' Locked on ',date);
+    //Try to remove .AT2 ext in the future
     f2 << "; " << fn1 << " Locked on " << (1900 + ltm->tm_year) << "/" << (1 + ltm->tm_mon) <<
        "/" << (ltm->tm_mday) << endl;
     f2 <<";------------------------------------------------------------------------------\n";
 
-    lock_code = "";
     //k:=random(21)+20;
+    k = rand() % 21 + 20;
+    auto *lock_code = new char[k];
 
-    //DECODE LOCK
-    for (i=1; i < k; i++){
-        //lock_code[i] = //char(ord(lock_code[i])-65);
+    //Generate lock code to char array
+    for (i = 0; i < k; i++) {
+        //lock_code[i] = static_cast<char>(rand() % 32 + 65);
+        lock_code[i] = (char)(rand() % 32 + 65);
     }
 
-    //write('Encoding "',fn1,'"...');
+    //Print lock code info
+    f2 << "#LOCK" << lock_type << " ";
+    for (i = 0; i < k; i++) {
+        f2 << lock_code[i];
+    }
+    f2 << endl;
+
+    //DECODE lock_code
+    for (i = 0; i < k; i++){
+        lock_code[i] = (char)((int)(lock_code[i]) - 65);
+    }
+
+    cout << "Encoding " << fn1 << "..." << endl;
 
     //ENCODE ROBOT
     //s = //btrim(s);
 
     //if length(s)>0 then
-    f2 << " "; // << //ucase(s));
+    if (s.length() > 0) {
+        write_line("", ucase(s), f2, k, lock_code);
+    }
 
     // while not eof(f1) do
     //readln(f1,s1); s:='';
     //s1:=btrim(ucase(s1));
 
-    //write_line(s,s1);
+    while (getline(f1, s1)) {
+        //Read line of f1 to s1, done in while cond. When it reaches EOF, while loop ends.
+
+        s = "";
+        s1 = ucase(s1);
+
+        write_line(s, s1, f2, k, lock_code);
+    }
 
     cout << "Done. Used LOCK Format #" << lock_type << "." << endl;
     cout << "Only ATR2 v2.08 or later can decode." << endl;
@@ -129,13 +157,17 @@ int main(int argc, char *argv[]) {
     f2.close();
 }
 
-int write_line(string s, string s1, ofstream f2) {
+int write_line(string s, string s1, ofstream &f2, int k, char lock_code[]) {
+    //cout << "write_line: " << s1 << endl;
+
     //s = prepare(s, s1);
+
+    //cout << "write_line s: " << s << endl;
 
     //write line
     if (s1.length() > 0) {
-        //s = encode(s);
-        f2 << s;
+        s = encode(s1, lock_code);
+        f2 << s << endl;
     }
 }
 
@@ -184,10 +216,13 @@ string ucase(string str) {
 string prepare(string s, string s1){
 //function prepare(s,s1:string):string;
 
-    int i,j,k,l;
+    int i,k;
     string s2;
 
-//    {--remove comments--}
+    cout << "prepare s: " << s << endl;
+    cout << "prepare s1: " << s1 << endl;
+
+    //{--remove comments--}
     if ((s1.length()==0) || (s1[1]=';')){
         s1="";
     }
@@ -225,6 +260,42 @@ string prepare(string s, string s1){
         s = s+s2;
     }
 
-    return s;
+    return s1;
 //    prepare=s;
+}
+
+string encode(string s, char lock_code[]) {
+    int i, lock_pos, this_dat, lock_dat;
+
+    int n = s.length();
+    char c[n+1];
+    strcpy(c, s.c_str());
+
+    cout << "encode: " << s << endl;
+
+    lock_pos = 0;
+    lock_dat = 0;
+    // if lock_code<>'' then ???
+    //if(lock_code='') {
+        for (i = 0; i < s.length(); i++) {
+            lock_pos++;
+
+            if (lock_pos > strlen(lock_code)) {
+                lock_pos = 0;
+            }
+
+            //If character is outside normal alphabet/symbols in ASCII range, change it to a space
+            if (((int)(c[i]) <= 31) || ((int)(c[i]) >= 128)) {
+                c[i] = ' ';
+            }
+
+            this_dat = (int)(c[i]) & 15;
+            //Debug:
+            cout << this_dat << endl;
+            c[i] = (char)(((int)(c[i]) ^ ((int)(lock_code[lock_pos]) ^ lock_dat)) + 1);
+            lock_dat = this_dat;
+        }
+        s = c;
+        return s;
+    //}
 }
