2bc2f8c422cac752cd2f9750fcddbab949415f8b
schultc3 <34105150+schultc3@users.noreply.github.com>
2018-02-26 10:50:56 -0500

    Add files via upload

---
+++ Connor's Functions/get_val.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+
+int main()
+{
+
+
+int i,j,k,l;
+while (k=0)
+{
+j=(code[c].op[max_op]) shr (4*o) + 15;
+i=code[c].op[o];
+   if (j = 1)
+   {
+    k=get_from_ram(n,i,j);
+    }
+    else
+    {
+    k=i
+    }
+   if (j >0) {
+    k=get_from_ram(n,k,j);
+  }
+ get_val=k;
+}
+}
---
+++ Connor's Functions/pop.cpp
@@ -0,0 +1,20 @@
+//function pop(n:integer):integer;
+
+using namespace std;
+
+int main()
+{
+
+int k ;
+while (robot[n])
+ {
+   if ((ram[71] > stack_base) || (ram[71] <= (stack_base + stack_size)))
+   {
+     dec(ram[71]);
+     k = ram[ram[71]];
+   }
+   else robot_error(n,5,cstr(ram[71]));
+ }
+ pop=k;
+ }
+
