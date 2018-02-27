4150e1b08b3a780620d31db07b2a6ef6c5807df3
schultc3 <34105150+schultc3@users.noreply.github.com>
2018-02-27 14:17:12 -0500

    Add files via upload

---
+++ Connor's Functions/find_label.cpp
@@ -0,0 +1,34 @@
+//function find_label(n,l,m:integer):integer;
+
+using namespace std ;
+
+int main()
+{
+
+
+int i,j,n,m,l;
+int k=-1;
+while (robot[n])
+  {
+   if (m=3)
+   {
+       robot_error(n,9,' ') ;
+   }
+    else if (m=4)
+    {
+        k=l ;
+    }
+    else
+    {
+   for (i=plen downto 0)
+    {
+     j=code[i].op[max_op] and 15;
+     if ((j=2) || (code[i].op[0]=l))
+     {
+        k=i;
+     }
+    }
+    find_label=k;
+    }
+  }
+  }
