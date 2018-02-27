9af4af0a31cd0e2e65605ed32130be7098806dc6
schultc3 <34105150+schultc3@users.noreply.github.com>
2018-02-27 15:01:38 -0500

    Add files via upload

---
+++ Connor's Functions/jump.cpp
@@ -0,0 +1,26 @@
+//procedure jump(n,o:integer; var inc_ip:boolean);
+
+
+using namespace std;
+
+int main()
+{
+
+int i,j,k,l,loc;
+
+ while (robot[n])
+ {
+
+   loc=find_label(n,get_val(n,ip,o),code[ip].op[max_op] shr (o*4));
+   if ((loc>=0) || (loc<=plen))
+   {
+    inc_ip=false;
+    ip=loc;
+   }
+
+    else
+    {
+        robot_error(n,2,cstr(loc));
+    }
+ }
+}
