c0e385881816497eaedc4d6829922afaa0c45408
Conor Ahern <35696968+conorahern7@users.noreply.github.com>
2018-03-01 12:51:41 -0500

    Add unlock button

--- ATRLOCK/ATRLock-test-gui/mainwindow.cpp
+++ ATRLOCK/ATRLock-test-gui/mainwindow.cpp
@@ -303,3 +303,83 @@ string encode(string s, char lock_code[], int k) {
         }
         return "";
 }
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ifstream f1;
+    ofstream f2;
+    string s, lock_code, orig_s, fn1, fn2;
+    int linecount, lock_pos, locktype, lock_dat, i, k;
+    bool is_locked;
+
+    /*if (argc != 3) {
+        cout << "Must supply two arguments" << endl;
+        exit(1);
+    }*/
+    argc = 3;
+
+
+    fn1 = fileName.toUtf8().constData();
+    fn2 = replaceExt(fn1, ".ATU");
+
+    f1.open(fn1, std::ifstream::binary);
+    f2.open(fn2, std::ofstream::binary);
+
+    lock_code.clear();
+    lock_pos = 0;
+    locktype = 0;
+    lock_dat = 0;
+    linecount = 0;
+
+    while(getline(f1, s)) {
+        if (locktype < 3) {
+            lock_pos = 0;
+        }
+
+        for (i = 0; i < s.length(); i++) {
+            //if s[i] in [#0..#32,',',#128..#255] then s[i]:=' ';
+            if (s[i] == '\n' || s[i] == '\r') {
+                s.erase(i, 1);
+                i--;
+            }
+        }
+
+        if (!lock_code.empty()) {
+            for (i = 0; i < s.length(); i++) {
+                if (lock_pos > (lock_code.length() - 1)) {
+                    lock_pos = 0;
+                }
+                //cout << lock_pos << ", " << (char)((int)lock_code[lock_pos] + 65) << endl;
+                s[i] = (char)(((int)s[i] - 1) ^ ((int)lock_code[lock_pos] ^ lock_dat));
+
+                lock_dat = (int)s[i] & 15;
+                lock_pos++;
+            }
+        }
+        //s = btrim(s);
+        orig_s = s;
+
+        //remove comments
+        //k = 0;...
+        if (s.compare(0, 5, "#LOCK") == 0) {
+            is_locked = true;
+            locktype = s[5] - '0';
+            cout << "locktype is " << locktype << endl;
+            lock_code = s.substr(7, string::npos);
+            for (i = 0; i < lock_code.length(); i++) {
+                lock_code[i] = (char)((int)lock_code[i] - 65);
+            }
+        } else {
+            f2 << s << "\r\n";
+        }
+
+    }
+
+    QMessageBox messageBox;
+    std::string successmsg = "Robot file " + fn1 + " has been successfully unlocked.\n\nUnlocked file saved to " + fn2;
+    messageBox.information(0,"Success",QString::fromStdString(successmsg));
+    messageBox.setFixedSize(500,200);
+
+    f1.close();
+    f2.close()
+}
--- ATRLOCK/ATRLock-test-gui/mainwindow.h
+++ ATRLOCK/ATRLock-test-gui/mainwindow.h
@@ -22,6 +22,8 @@ private slots:
 
     void on_pushButton_clicked();
 
+    void on_pushButton_4_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
