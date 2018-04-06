f2c9c6ef31816ce59c9b653c175794388e534a78
Conor Ahern <35696968+conorahern7@users.noreply.github.com>
2018-04-06 01:19:57 -0400

    Set QApp Style to Fusion for consistency across computers

--- ATROBS/main.cpp
+++ ATROBS/main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+
     MainWindow w;
     w.show();
 
