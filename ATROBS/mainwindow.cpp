b90719b8956dac6071d6dba9b26d188685e6cd05
Conor Ahern <35696968+conorahern7@users.noreply.github.com>
2018-04-09 11:08:14 -0400

    Robot browse buttons

--- ATROBS/mainwindow.cpp
+++ ATROBS/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for (int i = 0; i < 6; i++) {
+        fileName[i] = new QString("");
+    }
 }
 
 MainWindow::~MainWindow()
@@ -13,7 +18,40 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_13_clicked()
+std::string MainWindow::no_path(std::string fn) {
+    int i, k;
+
+    k = 0;
+    for (i = fn.length() - 1; i >= 0; i--) {
+        if(((fn[i] == '\\') || (fn[i] == ':') || (fn[i] == '/')) && (k < i)) {
+            k = i;
+        }
+    }
+    if (k != 0) {
+        return MainWindow::rstr(fn, fn.length() - k - 1);
+    } else {
+        return fn;
+    }
+}
+
+std::string MainWindow::rstr(std::string s1, int l) {
+    if ((int)s1.length() <= 1) {
+        return s1;
+    } else {
+        s1.erase(0, (s1.length() - l));
+        return s1;
+    }
+}
+
+void MainWindow::on_pushButton_13_clicked() //Quit button
 {
     exit(0);
+}
+
+void MainWindow::on_pushButton_clicked() //Robot 1 browse button
+{
+    *fileName[0] = QFileDialog::getOpenFileName(this, tr("Open File"),
+                                                    "",
+                                                    tr("ATRobot File (*.AT2)"));
+    ui->label_6->setText(QString::fromStdString(MainWindow::no_path(fileName[0]->toStdString())));
 }
--- ATROBS/mainwindow.h
+++ ATROBS/mainwindow.h
@@ -14,12 +14,19 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    std::string no_path(std::string fn);
+    std::string rstr(std::string s1, int l);
 
 private slots:
     void on_pushButton_13_clicked();
 
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    //QString *fileName[6];
+    QString **fileName = new QString*[6];
 };
 
 #endif // MAINWINDOW_H
