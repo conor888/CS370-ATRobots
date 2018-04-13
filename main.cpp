350d6e0907ecb09967b15ea23e94c8b945eeb2aa
Conor Ahern <35696968+conorahern7@users.noreply.github.com>
2018-04-13 19:01:12 -0400

    Add rest of ATR2 window

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 #include <QApplication>
 #include "arena.h"
 #include "atr2var.h"
+#include "window.h"
+#include <QGridLayout>
+#include "rgraph.h"
+#include "cgraph.h"
 
 int main(int argc, char *argv[]) {
 
@@ -24,12 +28,39 @@ int main(int argc, char *argv[]) {
 
     atr2var av;
 
-    arena atr2a(&av);
-    atr2a.setStyleSheet("background-color:black;");
-    atr2a.setAttribute(Qt::WA_OpaquePaintEvent, true);
-    atr2a.show();
+    arena *atr2a = new arena(&av);
+    atr2a->setStyleSheet("background-color:black;");
+    atr2a->setAttribute(Qt::WA_OpaquePaintEvent, true);
+    //atr2a.show();
 
-    WorkerThread *workerThread = new WorkerThread(&av, argc, args, &atr2a);
+    QGridLayout *atr2l = new QGridLayout();
+
+    rgraph **rgraphs = new rgraph*[6];
+
+    cgraph *cycleg = new cgraph(&av);
+    //one->setStyleSheet("background: rgb(255, 0, 0)");
+    //one->update();
+
+    window atr2w(&av, cycleg);
+    atr2w.setLayout(atr2l);
+
+    atr2l->addWidget(atr2a, 0, 0);
+
+    for (int i = 0; i < 6; i++) {
+        rgraphs[i] = new rgraph(i, &av);
+        atr2l->addWidget(rgraphs[i], i, 1);
+    }
+
+    atr2l->addWidget(cycleg, 6, 1);
+    atr2l->setSpacing(5);
+    atr2l->setRowStretch(0, 0);
+    atr2l->setRowStretch(6, 1);
+    atr2l->setContentsMargins(5, 5, 5, 5);
+
+    atr2w.setStyleSheet("background-color: rgb(168, 168, 168);");
+    atr2w.show();
+
+    WorkerThread *workerThread = new WorkerThread(&av, argc, args, atr2a, rgraphs, cycleg);
     //connect(workerThread, &WorkerThread::resultReady, this, &MyObject::handleResults);
     //connect(workerThread, &WorkerThread::finished, workerThread, &QObject::deleteLater);
     workerThread->start();
